c791676df831d8fbfaa6d9db831e4be2ad20d7c1
Vudang <danglegiavu@gmail.com>
2021-03-05 14:14:55 +0700

    Build codebase frontend react native

---
+++ src/db/Banners.js
@@ -0,0 +1,24 @@
+const banners = [
+  {
+    id: "9471645183",
+    imageUrl: require("../assets/Images/banner1.jpg"),
+  },
+  {
+    id: "5656210978",
+    imageUrl: require("../assets/Images/banner6.jpg"),
+  },
+  {
+    id: "044416421",
+    imageUrl: require("../assets/Images/banner3.jpg"),
+  },
+  {
+    id: "218716591",
+    imageUrl: require("../assets/Images/banner5.jpg"),
+  },
+  {
+    id: "218716523",
+    imageUrl: require("../assets/Images/banner4.jpg"),
+  },
+];
+
+export default banners;
---
+++ src/utils/Tools.js
@@ -0,0 +1,143 @@
+import React, { useCallback } from "react";
+import * as RootNavigation from "../navigation/RootNavigation";
+import { TouchableOpacity } from "react-native";
+import * as Linking from "expo-linking";
+import Colors from "./Colors";
+//Upload Image
+import * as ImagePicker from "expo-image-picker";
+import * as Permissions from "expo-permissions";
+import Constants from "expo-constants";
+import { STRIPE_PUBLISHABLE_KEY, API_URL } from "./Config";
+
+export const OpenURL = ({ url, children }) => {
+  const handlePress = useCallback(async () => {
+    // Checking if the link is supported for links with custom URL scheme.
+    const supported = await Linking.canOpenURL(url);
+
+    if (supported) {
+      // Opening the link with some app, if the URL scheme is "http" the web link should be opened
+      // by some browser in the mobile
+      await Linking.openURL(url);
+    } else {
+      Alert.alert(`Don't know how to open this URL: ${url}`);
+    }
+  }, [url]);
+  return <TouchableOpacity onPress={handlePress}>{children}</TouchableOpacity>;
+};
+
+//Handle Deep Link
+export const urlRedirect = (url) => {
+  if (!url) return;
+  // parse and redirect to new url
+  let { path, queryParams } = Linking.parse(url);
+  // console.log(
+  //   `Linked to app with path: ${path} and data: ${JSON.stringify(
+  //     queryParams
+  //   )}`
+  // );
+  if (path) {
+    RootNavigation.navigate(path, queryParams);
+  }
+  return;
+};
+
+//Handle Fetching timeout
+export const timeoutPromise = (url) => {
+  return new Promise((resolve, reject) => {
+    const timeoutId = setTimeout(() => {
+      reject(new Error("Timeout, Server is not responding"));
+    }, 20 * 1000);
+    url.then(
+      (res) => {
+        clearTimeout(timeoutId);
+        resolve(res);
+      },
+      (err) => {
+        clearTimeout(timeoutId);
+        reject(err);
+      }
+    );
+  });
+};
+
+export const _pickImage = async (action) => {
+  try {
+    if (Constants.platform.ios) {
+      const { status } = await Permissions.askAsync(
+        Permissions.CAMERA_ROLL,
+        Permissions.CAMERA
+      );
+      if (status !== "granted") {
+        return alert(
+          "Sorry, we need camera roll permissions to make this work!"
+        );
+      }
+    }
+    const type =
+      action === "library"
+        ? ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 4],
+            quality: 1,
+          })
+        : ImagePicker.launchCameraAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 4],
+            quality: 1,
+          });
+
+    let result = await type;
+    return result;
+  } catch (E) {
+    console.log(E);
+  }
+};
+
+export const colorCheck = (colorCode) => {
+  switch (colorCode) {
+    case "yellow":
+      return Colors.yellow;
+    case "green":
+      return Colors.green;
+    case "purple":
+      return Colors.purple;
+    case "blue":
+      return Colors.water;
+    case "pink":
+      return Colors.straw;
+    default:
+      return Colors.lighter_green;
+  }
+};
+
+//Get token from Stripe Server
+
+export const getCreditCardToken = (creditCardData) => {
+  const card = {
+    "card[number]": creditCardData.values.number.replace(/ /g, ""),
+    "card[exp_month]": creditCardData.values.expiry.split("/")[0],
+    "card[exp_year]": creditCardData.values.expiry.split("/")[1],
+    "card[cvc]": creditCardData.values.cvc,
+  };
+  return fetch("https://api.stripe.com/v1/tokens", {
+    headers: {
+      // Use the correct MIME type for your server
+      Accept: "application/json",
+      // Use the correct Content Type to send data to Stripe
+      "Content-Type": "application/x-www-form-urlencoded",
+      // Use the Stripe publishable key as Bearer
+      Authorization: `Bearer ${STRIPE_PUBLISHABLE_KEY}`,
+    },
+    // Use a proper HTTP method
+    method: "post",
+    // Format the credit card data to a string of key-value pairs
+    // divided by &
+    body: Object.keys(card)
+      .map((key) => key + "=" + card[key])
+      .join("&"),
+  })
+    .then((response) => response.json())
+    .catch((error) => console.log(error));
+};
