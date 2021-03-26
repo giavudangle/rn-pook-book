a2fbcde93d6d6189965d1ec0ffd82259e0ca21cf
Vudang <danglegiavu@gmail.com>
2021-03-26 20:53:14 +0700

    add image assets

--- App.js
+++ App.js
@@ -5,13 +5,13 @@ import { StyleSheet, Text, View, Dimensions, SafeAreaView, } from 'react-native'
 import IntroScreen from './src/screens/IntroScreen';
 import HomeScreen from './src/screens/HomeScreen';
 import LoginScreen from './src/screens/LoginScreen';
-import {SignUpScreen} from './src/screens/SignUpScreen'
+import {SignUpScreen} from './src/screens/SignupScreen'
 import {ForgetPasswordScreen} from './src/screens/ForgetPasswordScreen'
 const { width, height } = Dimensions.get('screen')
 
 export default function App() {
   return (
-   <ForgetPasswordScreen/>
+   <HomeScreen/>
   );
 }
 
