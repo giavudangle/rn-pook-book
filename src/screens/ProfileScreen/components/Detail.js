d9320e97da5946c6569441e88f470fb7a5ec4539
Vudang <danglegiavu@gmail.com>
2021-03-26 20:53:42 +0700

    Build Profile Screen

---
+++ src/screens/ProfileScreen/components/Detail.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {View,Text} from 'react-native'  
+import {MaterialCommunityIcons,MaterialIcons} from '@expo/vector-icons'
+import Colors from '../../../utils/Colors'
+
+// Check types of component (strongly type)
+import PropTypes from 'prop-types'
+
+const Detail = ({icon,content}) => {
+  const Icon = 
+    icon === 'location-on' || icon === 'person'
+      ? MaterialIcons
+      : MaterialCommunityIcons
+  
+  return (
+    <View style={styles.infoContainer}>
+      <Icon name={icon} size={28} color={Colors.grey} />
+      <CustomText style={styles.detailText}>{content}</CustomText>
+    </View>
+  )
+}
+
+Detail.propTypes = {
+  icon : PropTypes.string.isRequired,
+  content : PropTypes.string.isRequired
+}
+
+const styles = StyleSheet.create({
+  infoContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    borderBottomWidth: 1,
+    borderBottomColor: Colors.grey,
+    height: 60,
+    alignItems: "center",
+  },
+  detailText: {
+    fontWeight: "500",
+    color: Colors.text,
+  },
+});
+
+
+export default Detail;
