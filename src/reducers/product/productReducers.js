23635cfadab93d21c59250694276b82f28f4e935
Vudang <danglegiavu@gmail.com>
2021-03-27 18:35:46 +0700

    add product reducer

---
+++ src/reducers/product/productReducers.js
@@ -0,0 +1,15 @@
+import React from 'react'; 
+
+
+const initialState = {
+  products: [],
+  isFirstOpen: false,
+  isLoading: false,
+};
+
+export const productReducer = (state = initialState, action) => {
+  switch(action.type){
+    default:
+      return state
+  }
+}
