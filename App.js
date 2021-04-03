54909a358116f8e6ffe4836bd1eaaab487e9da07
PhanAnh1811 <tuananh.61779@gmail.com>
2021-04-03 09:39:04 +0700

    update

--- App.js
+++ App.js
@@ -7,11 +7,12 @@ import HomeScreen from './src/screens/HomeScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import {SignUpScreen} from './src/screens/SignUpScreen'
 import {ForgetPasswordScreen} from './src/screens/ForgetPasswordScreen'
+
 const { width, height } = Dimensions.get('screen')
 
 export default function App() {
   return (
-   <ForgetPasswordScreen/>
+   <IntroScreen/>
   );
 }
 
