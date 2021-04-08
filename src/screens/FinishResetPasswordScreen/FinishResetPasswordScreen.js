bcec9d165af21958e5fd9969e0cc361d31f49cb9
PhanAnh1811 <tuananh.61779@gmail.com>
2021-04-08 22:45:28 +0700

    Implement UI FinishResetPassWordScreen

--- src/screens/FinishResetPasswordScreen/FinishResetPasswordScreen.js
+++ src/screens/FinishResetPasswordScreen/FinishResetPasswordScreen.js
@@ -1,11 +1,81 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import { View, Text, StyleSheet, Dimensions, TouchableOpacity } from 'react-native';
+import LottieView from 'lottie-react-native';
+import CustomText from '../../components/UI/CustomText';
+import Colors from '../../utils/Colors';
 
-export const FinishResetPasswordScreen = () => {
+
+const { width, height } = Dimensions.get('screen');
+
+export const FinishResetPasswordScreen = (props) => {
+  console.log(props);
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.container}>
+
+      <View style={styles.info}>
+        <LottieView
+          source={require('../../components/IconAnimation/mail-done.json')}
+          autoPlay
+          loop={true}
+          resizeMode='cover'
+          style={{ height: 130 }}
+        />
+        <View style={{marginVertical:20}}>
+        <CustomText
+            style={{
+              fontSize:16,
+              color:Colors.blue,
+              textDecorationLine:'underline'
+            }}
+          >
+            tuananh.61779@gmail.com
+          </CustomText>
+        </View>
+      </View>
+
+      <View style={styles.id}>
+        <CustomText style={styles.title}>
+          Vui lòng kiểm tra hòm thư của bạn.
+        </CustomText>
+      </View>
+
+      <TouchableOpacity>
+        <View style={styles.button}>
+          <CustomText style={{...styles.title,color:'#fff'}}>
+            LOGIN
+        </CustomText>
+        </View>
+      </TouchableOpacity>
     </View>
   )
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+  },
+  info: {
+    marginTop: height / 4,
+    alignItems: 'center',
+  },
+  id: {
+    flexDirection: 'row',
+  },
+  title: {
+    fontSize: 20,
+    color: Colors.text,
+    fontWeight: '500',
+  },
+  button: {
+    marginTop:20,
+    backgroundColor:Colors.light_green,
+    borderRadius:10,
+    width:200,
+    height:45,
+    alignItems:'center',
+    justifyContent:'center',
+    
+  }
+})
