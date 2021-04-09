bd2694c8607d40ff4c4bec2b5568928ba8cb5cc9
PhanAnh1811 <tuananh.61779@gmail.com>
2021-04-09 22:47:47 +0700

    update

--- src/screens/SignupScreen/components/RenderField.js
+++ src/screens/SignupScreen/components/RenderField.js
@@ -1,39 +1,39 @@
-import React,{useState} from 'react';
-import { View, Text } from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
 import { TextInput } from 'react-native-paper';
 //Colors
 import Colors from '../../../utils/Colors';
 
-export default function RenderField({ 
-    label, 
-     iconRight, 
-     iconLeft,
-     keyBoardType,
-     autoCapitalize,
-     hide,
-     right,
-     }) {
-         
+export default function RenderField({
+    label,
+    iconLeft,
+    keyBoardType,
+    autoCapitalize,
+    hide,
+    right,
+}) {
+
     return (
         <View>
             <TextInput
-                style={{ marginBottom: 10,
-                    backgroundColor:'#fff',
-                 borderTopRightRadius:15,
-                    borderTopLeftRadius:15,
-                    borderBottomStartRadius:15,
-                    borderBottomEndRadius:15,
-                    width:340,
-                    height:61,
-                    alignSelf:'center',
-                    overflow:'hidden',
-                 }}
+                style={{
+                    marginBottom: 10,
+                    backgroundColor: '#fff',
+                    borderTopRightRadius: 15,
+                    borderTopLeftRadius: 15,
+                    borderBottomLeftRadius: 15,
+                    borderBottomRightRadius: 15,
+                    width: 340,
+                    height: 61,
+                    alignSelf: 'center',
+                    overflow: 'hidden',
+                }}
                 secureTextEntry={hide}
                 placeholder={label}
                 keyboardType={keyBoardType}
                 autoCapitalize={autoCapitalize}
-                 mode='flat'
-                theme={{colors:{primary:Colors.leave_green}}}
+                mode='flat'
+                theme={{ colors: { primary: Colors.leave_green } }}
                 right={right}
                 left={<TextInput.Icon
                     name={iconLeft}
@@ -41,7 +41,8 @@ export default function RenderField({
                     color={Colors.lighter_green}
                     style={{ paddingLeft: 10 }}
                 />}
+                onChangeText={text=>console.log(text)}
             />
         </View>
     )
-} 
+}
