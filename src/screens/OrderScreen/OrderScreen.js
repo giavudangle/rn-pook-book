548741f6e70df354156201f854e7f232c3cf958b
Vu Dang <danglegiavu@gmail.com>
2021-04-14 22:07:21 +0700

    Dev/vudang (#65)
    * add tools app
    
    * add config
    
    * add resuable component
    
    * implement component on ProfileScreen
    
    * update auth screne
    
    * update cart screen
    
    * update cart screen
    
    * update contact screen
    
    * update detail product screen
    
    * update favorite screen
    
    * update finish order screen
    
    * update reset password screen
    
    * update home screen
    
    * change intro screen code structures
    
    * update order screen
    
    * update payment screen
    
    * update pre order screen
    
    * add touch id screen
    
    * update forget password screen
    
    * update login screen
    
    * update product screen
    
    * update profile screen
    
    * update reset password screen
    
    * mapping signup
    
    * mapping reset password
    
    * re import const at app
    
    * add fonts
    
    * add authReducer
    
    * add cart reducer
    
    * add order reducer
    
    * favorite reducer
    
    * add product reducer
    
    * add local notification
    
    * export reducer
    
    * check first time open app
    
    * utils root ref
    
    * add navigator for all screens
    
    * add custom drawer
    
    * add app navigator
    
    * update global
    
    * update profile
    
    * Implemented TouchIdScreen
    
    * config app for testingg per screen
    
    * update ui profile screen
    
    * update pook navigator
    
    * add number format component
    
    * implment cart screen (UI only)
    
    * update profile screen
    
    * update config to backend
    
    * add utils
    
    * implement preorder screen
    
    * implement payment screen & add credit card screen (UI ONLY )
    
    * add animation stuffs
    
    * implement FinishOrderScreen
    
    * ok
    
    * update app.json (add splash )
    
    * Implement Order Screen (UI + mockData)
    
    * add order step component

--- src/screens/OrderScreen/OrderScreen.js
+++ src/screens/OrderScreen/OrderScreen.js
@@ -1,12 +1,129 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React,{useState} from 'react'
+import { View, Text ,StyleSheet,Dimensions} from 'react-native'
 
-export const OrderScreen = () => {
+import {useSelector} from 'react-redux'
+
+import {Header,OrderBody} from './components'
+
+
+// Mock DB
+const user = {
+  name:"Vudang"
+}
+
+const orders = [
+  {
+    "status": "waiting",
+    "_id": "60544c1161d79712005f47e1",
+    "totalAmount": "9999999",
+    "name": "Test payment",
+    "paymentMethod": "MasterCard",
+    "phone": "0967781273",
+    "address": "67 huynh thien loc",
+    "userId": null,
+    "items": [
+        {
+            "item": {
+                "url": "https://res.cloudinary.com/daktfdww5/image/upload/v1616142500/cemcxfewk9opzneq36h9.png",
+                "thumb": "https://res.cloudinary.com/daktfdww5/image/upload/v1616142502/dvjh8mwvnf5atgpqqzon.jpg",
+                "_id": "605449411d6e5b1185c9d2de",
+                "filename": "imageUrl-1616136513560.jpg",
+                "price": "999999",
+                "color": "red",
+                "origin": "USA",
+                "standard": "VIP",
+                "description": "A Best Book",
+                "type": "Sex",
+                "title": "Sex of Blow",
+                "createdAt": "2021-03-19T06:48:33.695Z",
+                "updatedAt": "2021-03-19T06:48:33.695Z",
+                "__v": 0
+            },
+            "quantity": "10"
+        }
+    ],
+    "createdAt": "2021-03-19T07:00:33.767Z",
+    "updatedAt": "2021-03-19T07:00:33.767Z",
+    "__v": 0
+  },
+  {
+    "status": "done",
+    "_id": "60544c1161d79712005f47e1",
+    "totalAmount": "9999999",
+    "name": "Test payment",
+    "paymentMethod": "MasterCard",
+    "phone": "0967781273",
+    "address": "67 huynh thien loc",
+    "userId": null,
+    "items": [
+        {
+            "item": {
+                "url": "https://res.cloudinary.com/daktfdww5/image/upload/v1616142500/cemcxfewk9opzneq36h9.png",
+                "thumb": "https://res.cloudinary.com/daktfdww5/image/upload/v1616142502/dvjh8mwvnf5atgpqqzon.jpg",
+                "_id": "605449411d6e5b1185c9d2de",
+                "filename": "imageUrl-1616136513560.jpg",
+                "price": "999999",
+                "color": "red",
+                "origin": "USA",
+                "standard": "VIP",
+                "description": "A Best Book",
+                "type": "Sex",
+                "title": "Sex of Blow",
+                "createdAt": "2021-03-19T06:48:33.695Z",
+                "updatedAt": "2021-03-19T06:48:33.695Z",
+                "__v": 0
+            },
+            "quantity": "10"
+        }
+    ],
+    "createdAt": "2021-03-19T07:00:33.767Z",
+    "updatedAt": "2021-03-19T07:00:33.767Z",
+    "__v": 0
+  },
+]
+
+const { height } = Dimensions.get("window");
+
+export const OrderScreen = ({navigation}) => {
+
+  // Connect to redux
+
+  const [isRefreshing,setIsRefreshing] = useState(false)
+ 
+
+  const loadOrders = async () => {
+
+  }
+
+
+
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.container}>
+      <Header navigation={navigation}/>
+      <OrderBody
+          user={user}
+          orders={orders}
+          isRefreshing={isRefreshing}
+          loadOrders={loadOrders}
+          navigation={navigation}
+      />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  centerLoader: {
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100%",
+    width: "100%",
+    position: "absolute",
+    top: Platform.OS === "android" ? 70 : height < 668 ? 70 : 90,
+  },
+});
+
 
 
