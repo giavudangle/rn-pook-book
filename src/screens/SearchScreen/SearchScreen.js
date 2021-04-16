1667f5c146c616f929c2ee32f81bb0080cfaad28
TranHyKhang <tranhykhang8108@gmail.com>
2021-04-16 11:20:55 +0700

    New screen SearchScreen

---
+++ src/screens/SearchScreen/SearchScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export function SearchScreen() {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
