129373ee312c1a3f5c87e33cef1f4e395c169322
TranHyKhang <tranhykhang8108@gmail.com>
2021-04-16 10:22:34 +0700

    implement body HomeScreen

--- src/screens/HomeScreen/components/Body.js
+++ src/screens/HomeScreen/components/Body.js
@@ -1,10 +1,21 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { 
+    View, 
+    Text, 
+    StyleSheet,
+    FlatList,
+    ScrollView 
+} from 'react-native'
 
 import Carousel from 'react-native-snap-carousel'
 
+//Colors
+import Colors from '../../../utils/Colors'
+
 //Component
 import RenderCarouselItem from './RenderCarouselItem'
+import RenderGridItem from './RenderGirdItem'
+
 
 const data = [
     'haha',
@@ -12,25 +23,302 @@ const data = [
     'hoho'
 ]
 
+const mockData = [
+    {
+        "url": "https://res.cloudinary.com/daktfdww5/image/upload/v1616142500/cemcxfewk9opzneq36h9.png",
+        "thumb": "https://res.cloudinary.com/daktfdww5/image/upload/v1616142502/dvjh8mwvnf5atgpqqzon.jpg",
+        "_id": "60544951d6e5b1185scaas9d2db",
+        "filename": "imageUrl-1616136453234.jpg",
+        "price": "999999",
+        "color": "red",
+        "origin": "USA",
+        "standard": "VIP",
+        "description": "A Best Book",
+        "type": "Sex",
+        "title": "Sex of Blow",
+        "createdAt": "2021-03-19T06:47:33.251Z",
+        "updatedAt": "2021-03-19T06:47:33.251Z",
+        "__v": 0
+    },
+    {
+      "url": "https://res.cloudinary.com/daktfdww5/image/upload/v1616142500/cemcxfewk9opzneq36h9.png",
+      "thumb": "https://res.cloudinary.com/daktfdww5/image/upload/v1616142502/dvjh8mwvnf5atgpqqzon.jpg",
+      "_id": "60544951d6e5b11asads85scaas9d2db",
+      "filename": "imageUrl-1616136453234.jpg",
+      "price": "999999",
+      "color": "red",
+      "origin": "USA",
+      "standard": "VIP",
+      "description": "A Best Book",
+      "type": "Sex",
+      "title": "Sex of Blow",
+      "createdAt": "2021-03-19T06:47:33.251Z",
+      "updatedAt": "2021-03-19T06:47:33.251Z",
+      "__v": 0
+  },
+    {
+      "url": "https://res.cloudinary.com/daktfdww5/image/upload/v1616142500/cemcxfewk9opzneq36h9.png",
+      "thumb": "https://res.cloudinary.com/daktfdww5/image/upload/v1616142502/dvjh8mwvnf5atgpqqzon.jpg",
+      "_id": "605449051d6e5b118549sdfd2db",
+      "filename": "imageUrl-1616136453234.jpg",
+      "price": "999999",
+      "color": "red",
+      "origin": "USA",
+      "standard": "VIP",
+      "description": "A Best Book",
+      "type": "Sex",
+      "title": "Sex of Blow",
+      "createdAt": "2021-03-19T06:47:33.251Z",
+      "updatedAt": "2021-03-19T06:47:33.251Z",
+      "__v": 0
+  },
+  {
+    "url": "https://res.cloudinary.com/daktfdww5/image/upload/v1616142500/cemcxfewk9opzneq36h9.png",
+    "thumb": "https://res.cloudinary.com/daktfdww5/image/upload/v1616142502/dvjh8mwvnf5atgpqqzon.jpg",
+    "_id": "60544901d6e5b1185c9cdbbdf2db",
+    "filename": "imageUrl-1616136453234.jpg",
+    "price": "999999",
+    "color": "red",
+    "origin": "USA",
+    "standard": "VIP",
+    "description": "A Best Book",
+    "type": "Sex",
+    "title": "Sex of Blow",
+    "createdAt": "2021-03-19T06:47:33.251Z",
+    "updatedAt": "2021-03-19T06:47:33.251Z",
+    "__v": 0
+  },
+  {
+    "url": "https://res.cloudinary.com/daktfdww5/image/upload/v1616142500/cemcxfewk9opzneq36h9.png",
+    "thumb": "https://res.cloudinary.com/daktfdww5/image/upload/v1616142502/dvjh8mwvnf5atgpqqzon.jpg",
+    "_id": "605449051d6e5b1185c9bfetddb",
+    "filename": "imageUrl-1616136453234.jpg",
+    "price": "999999",
+    "color": "red",
+    "origin": "USA",
+    "standard": "VIP",
+    "description": "A Best Book",
+    "type": "Sex",
+    "title": "Sex of Blow",
+    "createdAt": "2021-03-19T06:47:33.251Z",
+    "updatedAt": "2021-03-19T06:47:33.251Z",
+    "__v": 0
+  },
+  {
+    "url": "https://res.cloudinary.com/daktfdww5/image/upload/v1616142500/cemcxfewk9opzneq36h9.png",
+    "thumb": "https://res.cloudinary.com/daktfdww5/image/upload/v1616142502/dvjh8mwvnf5atgpqqzon.jpg",
+    "_id": "605449051d65b1185jc9dngs2db",
+    "filename": "imageUrl-1616136453234.jpg",
+    "price": "999999",
+    "color": "red",
+    "origin": "USA",
+    "standard": "VIP",
+    "description": "A Best Book",
+    "type": "Haha",
+    "title": "Sex of Blow",
+    "createdAt": "2021-03-19T06:47:33.251Z",
+    "updatedAt": "2021-03-19T06:47:33.251Z",
+    "__v": 0
+  },
+  {
+    "url": "https://res.cloudinary.com/daktfdww5/image/upload/v1616142500/cemcxfewk9opzneq36h9.png",
+    "thumb": "https://res.cloudinary.com/daktfdww5/image/upload/v1616142502/dvjh8mwvnf5atgpqqzon.jpg",
+    "_id": "605449051d6e5b1185c9fdhtesddb",
+    "filename": "imageUrl-1616136453234.jpg",
+    "price": "999999",
+    "color": "red",
+    "origin": "USA",
+    "standard": "VIP",
+    "description": "A Best Book",
+    "type": "Haha",
+    "title": "Sex of Blow",
+    "createdAt": "2021-03-19T06:47:33.251Z",
+    "updatedAt": "2021-03-19T06:47:33.251Z",
+    "__v": 0
+  },
+  {
+    "url": "https://res.cloudinary.com/daktfdww5/image/upload/v1616142500/cemcxfewk9opzneq36h9.png",
+    "thumb": "https://res.cloudinary.com/daktfdww5/image/upload/v1616142502/dvjh8mwvnf5atgpqqzon.jpg",
+    "_id": "605449051d6e5b1185cs9fdsd2db",
+    "filename": "imageUrl-161613645v3234.jpg",
+    "price": "999999",
+    "color": "red",
+    "origin": "USA",
+    "standard": "VIP",
+    "description": "A Best Book",
+    "type": "Haha",
+    "title": "Sex of Blow",
+    "createdAt": "2021-03-19T06:47:33.251Z",
+    "updatedAt": "2021-03-19T06:47:33.251Z",
+    "__v": 0
+  },
+  {
+    "url": "https://res.cloudinary.com/daktfdww5/image/upload/v1616142500/cemcxfewk9opzneq36h9.png",
+    "thumb": "https://res.cloudinary.com/daktfdww5/image/upload/v1616142502/dvjh8mwvnf5atgpqqzon.jpg",
+    "_id": "605449051d6e5b1185c9fdscxdsd2db",
+    "filename": "imageUrl-1616136453234.jpg",
+    "price": "999999",
+    "color": "red",
+    "origin": "USA",
+    "standard": "VIP",
+    "description": "A Best Book",
+    "type": "Haha",
+    "title": "Sex of Blow",
+    "createdAt": "2021-03-19T06:47:33.251Z",
+    "updatedAt": "2021-03-19T06:47:33.251Z",
+    "__v": 0
+  },
+  {
+    "url": "https://res.cloudinary.com/daktfdww5/image/upload/v1616142500/cemcxfewk9opzneq36h9.png",
+    "thumb": "https://res.cloudinary.com/daktfdww5/image/upload/v1616142502/dvjh8mwvnf5atgpqqzon.jpg",
+    "_id": "605449051d6e5b1185c9f4dhtesddb",
+    "filename": "imageUrl-1616136453234.jpg",
+    "price": "999999",
+    "color": "red",
+    "origin": "USA",
+    "standard": "VIP",
+    "description": "A Best Book",
+    "type": "Haha",
+    "title": "Sex of Blow",
+    "createdAt": "2021-03-19T06:47:33.251Z",
+    "updatedAt": "2021-03-19T06:47:33.251Z",
+    "__v": 0
+    },
+    {
+    "url": "https://res.cloudinary.com/daktfdww5/image/upload/v1616142500/cemcxfewk9opzneq36h9.png",
+    "thumb": "https://res.cloudinary.com/daktfdww5/image/upload/v1616142502/dvjh8mwvnf5atgpqqzon.jpg",
+    "_id": "605449051d6e5b1185cs69fdsd2db",
+    "filename": "imageUrl-161613645v3234.jpg",
+    "price": "999999",
+    "color": "red",
+    "origin": "USA",
+    "standard": "VIP",
+    "description": "A Best Book",
+    "type": "Haha",
+    "title": "Sex of Blow",
+    "createdAt": "2021-03-19T06:47:33.251Z",
+    "updatedAt": "2021-03-19T06:47:33.251Z",
+    "__v": 0
+    },
+    {
+    "url": "https://res.cloudinary.com/daktfdww5/image/upload/v1616142500/cemcxfewk9opzneq36h9.png",
+    "thumb": "https://res.cloudinary.com/daktfdww5/image/upload/v1616142502/dvjh8mwvnf5atgpqqzon.jpg",
+    "_id": "605449051d6e5b1185c92fdscxdsd2db",
+    "filename": "imageUrl-1616136453234.jpg",
+    "price": "999999",
+    "color": "red",
+    "origin": "USA",
+    "standard": "VIP",
+    "description": "A Best Book",
+    "type": "Haha",
+    "title": "Sex of Blow",
+    "createdAt": "2021-03-19T06:47:33.251Z",
+    "updatedAt": "2021-03-19T06:47:33.251Z",
+    "__v": 0
+    },
+  {
+    "url": "https://res.cloudinary.com/daktfdww5/image/upload/v1616142500/cemcxfewk9opzneq36h9.png",
+    "thumb": "https://res.cloudinary.com/daktfdww5/image/upload/v1616142502/dvjh8mwvnf5atgpqqzon.jpg",
+    "_id": "605449051d6e5b118549xcsdfd2db",
+    "filename": "imageUrl-1616136453234.jpg",
+    "price": "999999",
+    "color": "red",
+    "origin": "USA",
+    "standard": "VIP",
+    "description": "A Best Book",
+    "type": "hihi",
+    "title": "Sex of Blow",
+    "createdAt": "2021-03-19T06:47:33.251Z",
+    "updatedAt": "2021-03-19T06:47:33.251Z",
+    "__v": 0
+  },
+  {
+  "url": "https://res.cloudinary.com/daktfdww5/image/upload/v1616142500/cemcxfewk9opzneq36h9.png",
+  "thumb": "https://res.cloudinary.com/daktfdww5/image/upload/v1616142502/dvjh8mwvnf5atgpqqzon.jpg",
+  "_id": "60544901d6e5b1185c9cdbmtbdf2db",
+  "filename": "imageUrl-1616136453234.jpg",
+  "price": "999999",
+  "color": "red",
+  "origin": "USA",
+  "standard": "VIP",
+  "description": "A Best Book",
+  "type": "hihi",
+  "title": "Sex of Blow",
+  "createdAt": "2021-03-19T06:47:33.251Z",
+  "updatedAt": "2021-03-19T06:47:33.251Z",
+  "__v": 0
+  },
+  {
+  "url": "https://res.cloudinary.com/daktfdww5/image/upload/v1616142500/cemcxfewk9opzneq36h9.png",
+  "thumb": "https://res.cloudinary.com/daktfdww5/image/upload/v1616142502/dvjh8mwvnf5atgpqqzon.jpg",
+  "_id": "605449051d6e5b1185c9bdffetddb",
+  "filename": "imageUrl-1616136453234.jpg",
+  "price": "999999",
+  "color": "red",
+  "origin": "USA",
+  "standard": "VIP",
+  "description": "A Best Book",
+  "type": "hihi",
+  "title": "Sex of Blow",
+  "createdAt": "2021-03-19T06:47:33.251Z",
+  "updatedAt": "2021-03-19T06:47:33.251Z",
+  "__v": 0
+  },
+  {
+  "url": "https://res.cloudinary.com/daktfdww5/image/upload/v1616142500/cemcxfewk9opzneq36h9.png",
+  "thumb": "https://res.cloudinary.com/daktfdww5/image/upload/v1616142502/dvjh8mwvnf5atgpqqzon.jpg",
+  "_id": "605449051d65b1185jc9d7ngs2db",
+  "filename": "imageUrl-1616136453234.jpg",
+  "price": "999999",
+  "color": "red",
+  "origin": "USA",
+  "standard": "VIP",
+  "description": "A Best Book",
+  "type": "hihi",
+  "title": "Sex of Blow",
+  "createdAt": "2021-03-19T06:47:33.251Z",
+  "updatedAt": "2021-03-19T06:47:33.251Z",
+  "__v": 0
+  },
+]
+
 export default function Body() {
     let carousel = 0;
     return (
-        <View style={styles.container}>
+        <ScrollView style={styles.container}>
             <Carousel
                 ref={c => carousel = c}
                 data={data}
                 renderItem={({item}) => <RenderCarouselItem item={item}/>}
                 sliderWidth={400}
-                itemWidth={350}
+                itemWidth={370}
                 loop
-                layout='tinder'
             />
-        </View>
+            <Text 
+                style={{
+                    alignSelf: 'flex-start',
+                    marginLeft: 10,
+                    fontSize: 20,
+                    color: Colors.lighter_green,
+                    fontWeight: 'bold'
+                }}
+            >
+                Danh mục sách
+            </Text>
+           <View style={{alignItems: 'center'}}>
+            <FlatList
+                    data={mockData}
+                    keyExtractor={item => item._id}
+                    renderItem={({item}) => <RenderGridItem item={item}/>}
+                    numColumns={2}
+                    initialNumToRender={1}
+                />
+           </View>
+        </ScrollView>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        alignItems: 'center'
     }
 })
