5184cd9b3e98691ed197e4d6a9c0e517b81929e6
TranHyKhang <tranhykhang8108@gmail.com>
2021-04-17 14:42:32 +0700

    Update SearchScreen Empty Body

--- src/screens/SearchScreen/SearchScreen.js
+++ src/screens/SearchScreen/SearchScreen.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
+//styles
+import styles from './styles'
+
+//Components
+import Header from './components/Header'
+import Body from './components/Body'
+
 export function SearchScreen() {
     return (
-        <View>
-            <Text></Text>
+        <View style={styles.container}>
+            <Header/>
+            <Body/>
         </View>
     )
 }
