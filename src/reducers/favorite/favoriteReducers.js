c68e471d8622af32faefe9164e0016fd79fb6aaf
Vudang <danglegiavu@gmail.com>
2021-04-27 21:46:16 +0700

    add favorite reducer

--- src/reducers/favorite/favoriteReducers.js
+++ src/reducers/favorite/favoriteReducers.js
@@ -1,15 +1,49 @@
-import React from 'react'; 
-
-
+import {
+  FETCH_FAVORITE,
+  ADD_FAVORITE,
+  REMOVE_FAVORITE,
+  FAVORITE_LOADING,
+  FAVORITE_FAILURE,
+} from "../../@types/favoriteActionTypes";
 
 const initialState = {
   favoriteList: [],
   isLoading: false,
 };
-
 export const favoriteReducer = (state = initialState, action) => {
-  switch(action.type){
+  switch (action.type) {
+    case FAVORITE_LOADING:
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case FAVORITE_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+      };
+    case FETCH_FAVORITE:    
+      return {
+        ...state,
+        favoriteList: action.favoriteList,
+        isLoading: false,
+      };
+    case ADD_FAVORITE:
+      const newItem = action.addItem;
+      state.favoriteList.push(newItem);
+      return {
+        ...state,
+        isLoading: false,
+      };
+    case REMOVE_FAVORITE:
+      const id = action.itemId;
+      const newList = state.favoriteList.filter((item) => item._id !== id);
+      return {
+        ...state,
+        favoriteList: newList,
+        isLoading: false,
+      };
     default:
-      return state
+      return state;
   }
-}
+};
