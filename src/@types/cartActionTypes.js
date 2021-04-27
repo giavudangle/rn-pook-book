873784d91e6709044522550f0c2d43c4d531eb2f
Vudang <danglegiavu@gmail.com>
2021-04-27 21:44:05 +0700

    add types actions

---
+++ src/@types/cartActionTypes.js
@@ -0,0 +1,7 @@
+export const CART_LOADING = "CART_LOADING";
+export const CART_FAILURE = "CART_FAILURE";
+export const FETCH_CART = "FETCH_CART";
+export const ADD_CART = "ADD_CART";
+export const RESET_CART = "RESET_CART";
+export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
+export const DES_CART_QUANTITY = "DES_CART_QUANTITY";
