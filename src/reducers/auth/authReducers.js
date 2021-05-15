304b5fd875861735af29df0be81524e5bebb9f6c
PhanAnh1811 <tuananh.61779@gmail.com>
2021-05-15 15:30:53 +0700

    add case signup for authReducer

--- src/reducers/auth/authReducers.js
+++ src/reducers/auth/authReducers.js
@@ -2,45 +2,57 @@ import React from 'react';
 
 import UserMessages from "../../messages/user";
 
-
+import {AUTH_LOADING,
+  LOGIN,
+  LOGOUT,
+  EDIT_INFO,
+  UPLOAD_PROFILEPIC,
+  SIGN_UP,
+  AUTH_FAILURE,
+  FORGET_PASSWORD,
+  RESET_PASSWORD,} from '../../actions/auth/authActions'
 //types
 import {FIRST_OPEN} from '../../@types/firstTimeOpenActionTypes'
 
-// const initialState = {
-//   user: {},
-//   notification: {},
-//   isLoading: false,
-//   error: false,
-//   isFirstTime: false
-// };
-
-
-
 const initialState = {
-  user : {  
-    "userid": "6081a65c13f3e21db7724b3a",
-    "name": "Vudang",
-    "password": "$2a$10$EIL.gyeDLUlrcV7fqBbOOeq0UHa/dKxQsl8nygmPw3V10wiE7jOle",
-    "email": "vudang@gmail.com",
-    "phone": "",
-    "address": "",
-    "profilePicture": "",
-    "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MDgxYTY1YzEzZjNlMjFkYjc3MjRiM2EiLCJpYXQiOjE2MTk1MTE2NjAsImV4cCI6MTYyMDExNjQ2MH0.CUPn3j76NBZ1vd1QclaOA-2NdcCp84hbNBSGhbS7n3U",
-    "loginAt": 1619511660945,
-    "expireTime": 1620116460945
-  },
-  notification : UserMessages["user.login.success"],
-  isLoading:false,
-  error:false
+  user: {},
+  notification: {},
+  isLoading: false,
+  error: false,
+  isFirstTime: false
+};
 
-}
+
+
+// const initialState = {
+//   user : {  
+//     "userid": "6081a65c13f3e21db7724b3a",
+//     "name": "Vudang",
+//     "password": "$2a$10$EIL.gyeDLUlrcV7fqBbOOeq0UHa/dKxQsl8nygmPw3V10wiE7jOle",
+//     "email": "vudang@gmail.com",
+//     "phone": "",
+//     "address": "",
+//     "profilePicture": "",
+//     "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MDgxYTY1YzEzZjNlMjFkYjc3MjRiM2EiLCJpYXQiOjE2MTk1MTE2NjAsImV4cCI6MTYyMDExNjQ2MH0.CUPn3j76NBZ1vd1QclaOA-2NdcCp84hbNBSGhbS7n3U",
+//     "loginAt": 1619511660945,
+//     "expireTime": 1620116460945
+//   },
+//   notification : UserMessages["user.login.success"],
+//   isLoading:false,
+//   error:false
+
+// }
 
 export const authReducer = (state = initialState,action) => {
   switch(action.type){
     case FIRST_OPEN:
       return {...initialState, isFirstTime: true}
+    case SIGN_UP:
+      return{...state,isLoading:false}
     default:
       return state;
+    
+
   }
 
 }
