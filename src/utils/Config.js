8828ce91b255f54e844aa7826547cf0fc8b34f7d
Vudang <danglegiavu@gmail.com>
2021-05-23 14:24:46 +0700

    add skeleton cart

--- src/utils/Config.js
+++ src/utils/Config.js
@@ -1,7 +1,7 @@
 // API URL
 //export const API_URL = "codingwithvudang-bookserver.herokuapp.com/api/v1";
 //export const API_URL = "http://127.0.0.1:8080/api/v1"
-export const API_URL = "http://9aa5e27322d5.ngrok.io/api/v1"
+export const API_URL = "http://1ae51ed0b23a.ngrok.io/api/v1"
 
 export const secretKey = "VUDANG_SECRET_TOKEN";
 export const STRIPE_PUBLISHABLE_KEY = "sk_test_51IVWZTBKJhKXYzYCOPvgPcpsnuQUTBfNUYpJiGhnO3OhJVylGGdafi8j0gHkwTQBv6usQL0QXSom2y6a4BaOK8On00RBLOG1Kb"
